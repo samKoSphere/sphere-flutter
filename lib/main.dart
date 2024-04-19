8c207e5748afa1fb219fb1363a45cfbdf3d1dd54
samKoSphere <139430653+samKoSphere@users.noreply.github.com>
2024-04-19 14:18:45 +0800

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'app.dart';
 
 void main() {
@@ -10,12 +10,12 @@ void main() {
   // Todo: Add Widgets Binding
 
   // Todo: Initialize Firebase
-  // WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  // FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   // Load all the Material Design / Themes / Localizations / Bindings
 // Delay for 2 seconds and then continue with runApp()
   Future.delayed(const Duration(seconds: 1), () {
-    // FlutterNativeSplash.remove();
+    FlutterNativeSplash.remove();
     runApp(const App());
   });
 }
